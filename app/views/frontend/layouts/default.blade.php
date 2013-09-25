71c611e8727d2554b1c2be0b5c2895db6906d4c9
Kyle Gawryluk <kgawryluk@redglaze.com>
2013-09-24 21:30:03 -0700

    Boom

---
+++ app/views/frontend/layouts/default.blade.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<!-- Basic Page Needs
+		================================================== -->
+		<meta charset="utf-8" />
+		<title>
+			@section('title')
+			Bootstrap
+			@show
+		</title>
+		<meta name="keywords" content="your, awesome, keywords, here" />
+		<meta name="author" content="Jon Doe" />
+		<meta name="description" content="Lorem ipsum dolor sit amet, nihil fabulas et sea, nam posse menandri scripserit no, mei." />
+
+		<!-- Mobile Specific Metas
+		================================================== -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- CSS
+		================================================== -->
+		<link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet">
+		<link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+
+		<style>
+		@section('styles')
+		body {
+			padding: 10px 0;
+		}
+		/*@show*/
+		</style>
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!-- Favicons
+		================================================== -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('assets/ico/apple-touch-icon-144-precomposed.png') }}">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('assets/ico/apple-touch-icon-114-precomposed.png') }}">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}">
+		<link rel="apple-touch-icon-precomposed" href="{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}">
+		<link rel="shortcut icon" href="{{ asset('assets/ico/favicon.png') }}">
+	</head>
+
+	<body>
+
+		<a class="logo" href="#"><img src="{{asset('assets/img/logo.png')}}"></a>
+		<!-- Container -->
+		<div class="container">
+			<!-- Navbar -->
+			<div class="navbar navbar-inverse navbar-fixed-top">
+				<div class="navbar-inner">
+					<div class="container">
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
+						<a class="brand" href="#"></a>
+						<div class="nav-collapse collapse">
+							<ul class="nav pull-right">
+								@if (Sentry::check())
+
+								<li class="dropdown{{ (Request::is('account*') ? ' active' : '') }}">
+									<a class="dropdown-toggle" id="dLabel" role="button" data-toggle="dropdown" data-target="#" href="{{ route('account') }}">
+										Welcome, {{ Sentry::getUser()->first_name }}
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+										@if(Sentry::getUser()->hasAccess('admin'))
+										<li><a href="{{ route('admin') }}"><i class="icon-cog"></i> Administration</a></li>
+										@endif
+										<li{{ (Request::is('account/profile') ? ' class="active"' : '') }}><a href="{{ route('profile') }}"><i class="icon-user"></i> Your profile</a></li>
+										<li class="divider"></li>
+										<li><a href="{{ route('logout') }}"><i class="icon-off"></i> Logout</a></li>
+									</ul>
+								</li>
+								@else
+								<li {{ (Request::is('auth/signin') ? 'class="active"' : '') }}><a href="{{ route('signin') }}">Sign in</a></li>
+								<li {{ (Request::is('auth/signup') ? 'class="active"' : '') }}><a href="{{ route('signup') }}">Register</a></li>
+								@endif
+							</ul>
+							<div class="clearfix"></div>
+							<ul class="nav pull-right">
+								<li {{ (Request::is('/') ? 'class="active"' : '') }}><a href="{{ route('home') }}"><i class="icon-home icon-white"></i> Home</a></li>
+								<li {{ (Request::is('about-us') ? 'class="active"' : '') }}><a href="{{ URL::to('about-us') }}"><i class="icon-file icon-white"></i> What We Believe</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										Education
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li {{ (Request::is('about-us') ? 'class="active"' : '') }}><a href="{{ URL::to('about-us') }}"><i class="icon-file icon-white"></i> Bible College</a></li>
+										<li {{ (Request::is('about-us') ? 'class="active"' : '') }}><a href="{{ URL::to('about-us') }}"><i class="icon-file icon-white"></i> Grace Christian Home</a></li>
+									</ul>
+								</li>
+								
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										About
+										<b class="caret"></b>
+									</a>
+									<ul class="dropdown-menu">
+										<li {{ (Request::is('about-us') ? 'class="active"' : '') }}><a href="{{ URL::to('about-us') }}"><i class="icon-file icon-white"></i> About Babu</a></li>
+										<li {{ (Request::is('about-us') ? 'class="active"' : '') }}><a href="{{ URL::to('about-us') }}"><i class="icon-file icon-white"></i> About India</a></li>
+									</ul>
+								</li>
+								
+								<li {{ (Request::is('about-us') ? 'class="active"' : '') }}><a href="{{ URL::to('about-us') }}"><i class="icon-file icon-white"></i> Projects</a></li>
+								<li {{ (Request::is('contact-us') ? 'class="active"' : '') }}><a href="{{ URL::to('contact-us') }}"><i class="icon-file icon-white"></i> Contact us</a></li>
+							</ul>
+
+							
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Notifications -->
+			@include('frontend/notifications')
+			<div class="container white thumbnail">
+				@yield('content')	
+			</div>
+			
+
+
+			<!-- Footer -->
+			<footer class="navbar navbar-inverse navbar-fixed-bottom pad-footer">
+				<div class="navbar-inner">
+					<div class="container">
+						<p class="footer">&copy; Grace Christian Ministries {{ date('Y') }}</p>
+					</div>
+				</div>
+			</footer>
+		</div>
+
+		<!-- Javascripts
+		================================================== -->
+		<script src="{{ asset('assets/js/jquery.1.10.2.min.js') }}"></script>
+		<script src="{{ asset('assets/js/bootstrap/bootstrap.min.js') }}"></script>
+	</body>
+	</html>
---
+++ app/views/index.blade.php
@@ -0,0 +1,57 @@
+@extends('frontend/layouts/default')
+
+@section('content')
+<div class="row-fluid">
+    <div class="span4">
+        <img src="{{asset('assets/img/kids.jpg')}}" alt="">
+    </div>
+    <div class="span8 white-text">
+        <p>
+            Grace Christian Ministry is a mission in Pathanapuram, in the state of Kerala, India. 
+            GCM was founded and is maintained by Babu Samuel.
+        </p>
+        <p>
+            Babu Samuel is a native of Kerala State in southern India. He became a Christian in 1977.  As he received a call by God into ministry, he entered a Bible College in Madras, India, where he studied for four years graduating with a Bachelor of Theology degree.
+        </p>
+        <p>
+            Grace Christian College is the answer to long prayers to meet the growing demands for trained preachers and teachers for this ministry. Grace Christian College was created to prepare Christian workers to help spread New Testament Christianity throughout India.
+        </p>
+    </div>
+</div>
+<hr>
+<ul class="thumbnails">
+    <li class="span4">
+        <div class="thumbnail white-menu">
+            <a class="white-text" href="">
+                <img src="{{asset('assets/img/graduation.jpg')}}" alt="">
+                <h3>Bible College</h3>
+                <p>
+                    There are currently 20 students at the college. This number includes first and second year students.  At present, a student must complete 24 subjects within the three-year program.
+                </p>
+            </a>
+        </div>
+    </li>
+    <li class="span4">
+        <div class="thumbnail white-menu">
+            <a class="white-text" href="">
+                <img src="{{asset('assets/img/believe.jpg')}}" alt="">
+                <h3>Babu's Blog</h3>
+                <p>
+                    There are currently 20 students at the college. This number includes first and second year students.  At present, a student must complete 24 subjects within the three-year program.
+                </p>
+            </a>
+        </div>
+    </li>
+    <li class="span4">
+        <div class="thumbnail white-menu">
+            <a class="white-text" href="">
+                <img src="{{asset('assets/img/GCM Cover.jpg')}}" alt="">
+                <h3>Contribute</h3>
+                <p>
+                    There are currently 20 students at the college. This number includes first and second year students.  At present, a student must complete 24 subjects within the three-year program.
+                </p>
+            </a>
+        </div>
+    </li>
+</ul>
+@stop
